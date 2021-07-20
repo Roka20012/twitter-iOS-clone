d9f29b572995a679e3ec55b15b69a8ed5ce0a96a
Rostyslav Broslavskyi <r.broslavskyi@globallogic.com>
2021-07-20 23:23:01 +0300

    init commit

---
+++ twitter-clone/Controller/MainTabController.swift
@@ -0,0 +1,27 @@
+//
+//  MainTabController.swift
+//  twitter-clone
+//
+//  Created by Rostyslav Broslavskyi on 20.07.2021.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    // MARK: - Helpers
+    
+    func configureViewControllers() {
+        
+    }
+}
